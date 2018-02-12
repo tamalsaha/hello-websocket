28f82ac5e5b90efb71b556e787da10371e87e292
tamal <tamal@appscode.com>
2018-02-11 16:45:30 -0800

    Add installer yaml

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -45,7 +45,7 @@ FROM alpine
 RUN set -x \
   && apk add --update --no-cache ca-certificates
 
-COPY home.html /usr/bin/home.html
+COPY home.html /home.html
 COPY hello-websocket /usr/bin/hello-websocket
 
 USER nobody:nobody
